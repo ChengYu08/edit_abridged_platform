7e4237c7b7e6bdd2417ebd65718585f494504631
ChengYu08 <318771301@qq.com>
2024-04-16 15:59:19 +0800

    feat: ✨ 第二个版本

--- index.js
+++ index.js
@@ -4,24 +4,57 @@ var path = require("path");
 var logger = require("morgan");
 const fs = require("fs-extra");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+const md5 = require("md5");
 
 app.get("/write", function (req, res) {
-  var name = req.query.name;
   var content = req.query.content;
-  
-  writeFile(`./public/html/${name}`, content ?? "");
-  res.send(`${name}写入成功 \n 内容如下 ${content}`);
+  var name = req.query.name;
+  const id = md5(content);
+  const configJson = fs.readJSONSync("./public/config.json");
+  configJson[id] = {
+    name: `${name??id}`,
+    id,
+    url: `http://${req.hostname}:${req.socket.localPort}/show/${id}`,
+  };
+  fs.writeJSONSync("./public/config.json", configJson);
+  console.log(id);
+  writeFile(`./public/files/${id}.html`, content ?? "");
+  res.json({
+    id,
+    name: name??id,
+    path: `http://${req.hostname}:${req.socket.localPort}/show/${id}`,
+  });
 });
 
-app.get("/read",  function (req, res)  {
-  fs.readdirSync("./public/html").forEach(function (file) {
-    console.log(file);
-  })
-    res.send("Hello World");
+// 接口用于网页浏览
+app.get("/show/:id", function (req, res) {
+  const id = req.params.id;
+  const configJson = fs.readJSONSync("./public/config.json");
+  if (configJson[id] == undefined) {
+    res.send("网页不存在");
+    return;
+  }
+  const content = fs.readFileSync(`./public/files/${id}.html`).toString();
+  res.setHeader("Content-Type", "text/html");
+  res.send(content);
+});
 
+// 读取指定文件路径
+app.get("/read", function (req, res) {
+  const id = req.query.id;
+  if (fs.pathExistsSync(`./public/files/${id}.html`)) {
+    res.json({
+      url: `http://${req.hostname}:${req.socket.localPort}/files/${id}.html`,
+    });
+  } else {
+    res.json({ url: null, message: "文件不存在" });
+  }
+});
+
+// 获取列表
+app.get("/list", function (req, res) {
+  const currentList = fs.readJSONSync("./public/config.json");
+  res.json(currentList);
 });
 
 function writeFile(path, data) {
